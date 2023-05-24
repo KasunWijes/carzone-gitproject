424d52bf1a89816d409c9e3e0f8fefea3adc2959
Kasun <kasunpamodya@gmail.com>
2023-05-24 16:04:28 +0200

    added Base files and navigation files

--- pages/urls.py
+++ pages/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='home'),#'' means the main menue or home page
+    path('about', views.about, name='about'),  #views.method name is about/ name='about' name of our url
+    path('services', views.services, name='services'),  #comma at the end is must
+    path('contact', views.contact, name='contact'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'pages/home.html')
+
+
+def about(request): #pass a request to the method
+    return render(request, 'pages/about.html') #pass again request and about.html path to render method to url folder
+
+def services(request):
+    return render(request, 'pages/services.html')
+
+
+def contact(request):
+    return render(request, 'pages/contact.html')
